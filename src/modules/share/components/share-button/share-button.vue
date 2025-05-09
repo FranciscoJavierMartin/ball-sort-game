2d5843dbf7683fb108eea49c164be32653ed93fa
Francisco <github.backstage538@passmail.net>
2025-05-09 19:21:23 +0200

    Rename file

--- src/modules/share/components/share-button/share-button.vue
+++ src/modules/share/components/share-button/share-button.vue
@@ -1,12 +1,12 @@
 <template>
   <slot :onClick />
-  <ShareModal v-if="isModalVisible" :data @close="onCloseModal" />
+  <ShareModal v-if="isModalVisible" :data @closeModal="onCloseModal" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { ShareData } from '@/modules/share/interfaces/share-data';
-import ShareModal from '@/modules/share/components/modal/Modal.vue';
+import ShareModal from '@/modules/share/components/share-modal/share-modal.vue';
 import { shareLink } from '@/modules/share/helpers/share-link';
 
 const isModalVisible = ref<boolean>(false);
