d12069e71059d6fcff136126b34b0f2b0ee09e91
Francisco <github.backstage538@passmail.net>
2025-05-10 19:26:31 +0200

    Remove variable

--- src/modules/game/helpers/get-initial-balls.ts
+++ src/modules/game/helpers/get-initial-balls.ts
@@ -2,17 +2,17 @@ import COLORS_BALLS from '@/modules/common/helpers/colors';
 import type { Balls, TubesType } from '@/modules/common/interfaces/common';
 
 export default function getInitialBalls(tubes: TubesType): Balls[] {
-  let i: number = 0;
-
-  return Object.entries(tubes).flatMap(([tube, { balls = [] }]) => {
-    return balls.map<Balls>((ball, index: number) => ({
-      index: i++,
-      indexTube: +tube,
-      incognito: ball.incognito || false,
-      animate: false,
-      positionTube: index,
-      color: ball.value,
-      colors: COLORS_BALLS[ball.value],
-    }));
-  });
+  return Object.entries(tubes).flatMap(
+    ([tube, { balls = [] }], tubeIndex: number) => {
+      return balls.map<Balls>((ball, index: number) => ({
+        index: index + tubeIndex,
+        indexTube: +tube,
+        incognito: ball.incognito || false,
+        animate: false,
+        positionTube: index,
+        color: ball.value,
+        colors: COLORS_BALLS[ball.value],
+      }));
+    },
+  );
 }
